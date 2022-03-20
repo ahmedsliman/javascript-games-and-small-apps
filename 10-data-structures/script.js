b6acea05ff3b7aaab548ffb3dbda78fd1581d1cc
ahmed soliman <ahmedsliman@gmail.com>
2022-03-20 13:55:50 +0200

    destructuring arrays

--- 10-data-structures/script.js
+++ 10-data-structures/script.js
@@ -4,8 +4,12 @@ const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+
+  meal: function () {
+    return [this.mainMenu[0], this.starterMenu[0]];
+  },
 
   openingHours: {
     thu: {
@@ -22,3 +26,16 @@ const restaurant = {
     },
   },
 };
+
+const egyRest = restaurant.meal();
+// console.log(egyRest);
+
+// Array nested destructuring
+const nested = [1, 2, [4, 5], 6];
+const [j, , [x, y]] = nested;
+//console.log(j, x, y);
+
+// Default values
+const defaultV = [8];
+const [v, p = 1] = defaultV;
+console.log(v, p);
