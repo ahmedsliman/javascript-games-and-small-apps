3fad2b070bfd03c81931089119139b81745c52a6
ahmed soliman <ahmedsliman@gmail.com>
2022-03-21 14:05:18 +0200

    Higher order functions

---
+++ 10-functions/functions.js
@@ -0,0 +1,25 @@
+const oneWord = (str) => str.replace(/ /g, '').toLowerCase();
+const upperFirstWord = (str) => {
+    const [first, ...rest] = str.split(' ');
+    return [first.toUpperCase(), ...rest].join(' ');
+}
+
+const str2 = (str, fn) => {
+    console.log(`Original Text: ${str}`);
+    console.log(`Transformed Text: ${fn(str)}`);
+    console.log(`Function Name: ${fn.name}`);
+};
+
+// str2('ahmed soliman', oneWord);
+// str2('ahmed soliman', upperFirstWord);
+
+// Return function
+// const greet = function (greeting) {
+//     return function (name) {
+//         console.log(`${greeting} ${name}`);
+//     }
+// }
+
+const greet = greeting => name => console.log(`${greeting} ${name}`);
+
+greet('Hi')('Soly!');
