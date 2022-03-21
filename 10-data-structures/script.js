ba10945bc0265e6d24d0acab968a2c75f817a5f2
ahmed soliman <ahmedsliman@gmail.com>
2022-03-21 13:23:28 +0200

    DS

--- 10-data-structures/script.js
+++ 10-data-structures/script.js
@@ -1,33 +1,54 @@
 'use strict';
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  meal: function () {
-    return [this.mainMenu[0], this.starterMenu[0]];
+  // ES6 enhanced object literals
+  openingHours,
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
   },
 };
 
-const egyRest = restaurant.meal();
+// const egyRest = restaurant.openingHours();
 // console.log(egyRest);
 
 // Array nested destructuring
@@ -38,4 +59,14 @@ const [j, , [x, y]] = nested;
 // Default values
 const defaultV = [8];
 const [v, p = 1] = defaultV;
-console.log(v, p);
+//console.log(v, p);
+
+const restaurantCopy = {...restaurant};
+// console.log(restaurantCopy);
+
+// Rest Pattern
+const [a, b, ...arr] = [1, 2, 3, 4, 6];
+console.log(arr);
+
+const {sat, ...Weekdays} = restaurant.openingHours;
+console.log(Weekdays);
