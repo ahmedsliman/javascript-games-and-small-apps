7b8b8fec2f17da70f25cca46cf47a05bf2bc56a1
ahmed soliman <ahmedsliman@gmail.com>
2022-04-03 14:19:37 +0200

    Async - HttpRequest - Callbacks - Callback Hell

--- 12-async/script.js
+++ 12-async/script.js
@@ -4,3 +4,49 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+// console.log(1);
+// console.log(2);
+
+// // If will not block because it's async function (time functions in JS)
+// setTimeout(() => {
+//     console.log('callback function fired!');
+// }, 2000);
+
+// console.log(3);
+// console.log(4);
+
+//##### HttpRequest the old way
+const getTodos = (resource, callback) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 200) {
+            const data = JSON.parse(request.responseText);
+            callback(undefined, data);
+        } else if (request.readyState === 4) {
+            callback('Issue in the request!', undefined);
+        }
+    });
+
+    request.open('GET', resource);
+    request.send();
+};
+
+// getTodos('todos.json', (err, data) => {
+//     if (err) {
+//         console.log("can't fetch the data!");
+//     } else {
+//         console.log(data);
+//     }
+// });
+
+getTodos('todos.json', (err, data) => {
+    console.log(data);
+    getTodos('todos2.json', (err, data) => {
+        console.log(data);
+        getTodos('todos3.json', (err, data) => {
+            console.log(data);
+        });
+    });
+});
