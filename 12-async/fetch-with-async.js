b6d04e1b4daa89374ac19b29e45f6b92f36ddf00
ahmed soliman <ahmedsliman@gmail.com>
2022-04-03 15:46:17 +0200

    Fetch API - With await - throw  error

--- 12-async/fetch-with-async.js
+++ 12-async/fetch-with-async.js
@@ -1,5 +1,11 @@
 const getTodos = async () => {
     const response = await fetch('todos/todos.json');
+
+    // Promise will return reject and will be catched in catch
+    if (response.status !== 200) {
+        throw new Error('Issue in file!');
+    }
+
     const data = await response.json();
     return data;
 };
@@ -9,7 +15,7 @@ console.log(2);
 
 getTodos()
 .then(data => console.log('resolved:', data))
-.catch(err => console.log('rejected:', err));
+.catch(err => console.log('rejected:', err.message));
 
 console.log(3);
 console.log(4);
